d3611816d809fdf4d7362e5fe13c2b47cb5a3bd0
cjones0429 <cejones1997@aim.com>
2021-06-25 12:38:26 -0400

    added comments to render section of apps.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar/searchBar';
+import Comments from './Comments/comments';
 import apiKeys from '../API-Keys.json'
 
 
@@ -11,7 +12,7 @@ class App extends Component {
             videoId: '2RQh0BOoBbQ',
             relatedVideoIds: [],
             videoTitle: '',
-            // videoDescription: ''
+            videoDescription: ''
         }
     }
 
@@ -34,7 +35,7 @@ class App extends Component {
             videoId: videos.items[0].id.videoId,
             relatedVideoIds: relatedVideoIds,
             videoTitle: videos.items[0].title,
-            // videoDescription: videos.items[0].description,
+            videoDescription: videos.items[0].description,
         })
     }
 
@@ -60,11 +61,15 @@ class App extends Component {
                 <div>
                     <h2>{this.state.videoTitle}</h2>
                 </div>
-
-                <iframe id="ytplayer" type="text/html" width="640" height="360"
+                <br />
+                <iframe title="title" id="ytplayer" type="text/html" width="640" height="360"
                 src={`https://www.youtube.com/embed/${this.state.videoId}?autoplay=1&origin=http://example.com`}
                 frameBorder="0">
+                <br/>
                 </iframe>
+                <p>{this.state.videoDescription}</p>
+                <br/>
+                <Comments />
             </div>
          );
     }
